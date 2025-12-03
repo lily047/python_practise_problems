e93c3d82ba3167b424df1a167766a4bac2caba3c
lily047 <173460097+lily047@users.noreply.github.com>
2025-12-03 20:37:34 +0530

    Update new_concepts.py

--- new_concepts.py
+++ new_concepts.py
@@ -3,3 +3,9 @@ print(hash((1,2)))
 
 arr = map(int, input().split())
 #converts each str into an int 
+
+name, *line = input().split()
+#*variable collects all remaining items after the variables on the left have taken their share.
+
+print(f"{average_marks:.2f}")
+#to force the ouput to be 2 decimal places, by round(x,2) u get a float and by the above method u get a string 
